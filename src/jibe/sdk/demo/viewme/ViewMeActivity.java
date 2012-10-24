cca6606e01adfd987201a5cea9035052bd0d16b5
Trenton Johnson <trenton.johnson@sv.cmu.edu>
2012-10-24 11:00:37 -0700

    made video bigger

--- src/jibe/sdk/demo/viewme/ViewMeActivity.java
+++ src/jibe/sdk/demo/viewme/ViewMeActivity.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import jibe.sdk.demo.viewme.*;
 import jibe.sdk.client.JibeIntents;
 import jibe.sdk.client.JibeServiceListener.ConnectFailedReason;
 import jibe.sdk.client.events.JibeSessionEvent;
